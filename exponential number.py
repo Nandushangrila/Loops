f4ef962468f2f5ef2bdf6a1364a8d97213c263f5
Nandushangrila <nivedr06@gmail.com>
2025-10-25 12:46:20 -0500

    initial commit

---
+++ exponential number.py
@@ -0,0 +1,5 @@
+n=int(input("Enter any number that you would like to exponentially multiply by 4:"))
+
+exponent_number=n**4
+
+print("The answer to your number exponentially multiplied by 4 is",exponent_number)
